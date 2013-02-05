ed199079c0d8093d0d7bca656d87602fa54bd72b
Jindrich Mynarz <mynarzjindrich@gmail.com>
2013-02-05 15:40:59 +0100

    Notifications

--- convert.sh
+++ convert.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+notify () {
+  echo -e "\n$1\n"
+}
+
 # Check if all dependencies are satisfied
 for command in "arq" "curl" "rapper" "saxon" "unzip"
 do
@@ -13,41 +17,41 @@ if [ -d "temp" ]; then
   rm -r temp
 fi
 
-echo "\n%% Downloading CPV codes..."
+notify "Downloading CPV codes..."
 curl -O http://simap.europa.eu/news/new-cpv/cpv_2008_xml.zip
 
-echo "\n%% Unzipping..."
+notify "Unzipping..."
 unzip -d temp cpv_2008_xml.zip
 rm cpv_2008_xml.zip
 
-echo "\n%% Executing XSL transformations..."
+notify "Executing XSL transformations..."
 saxon -s:temp/cpv_2008.xml -xsl:cpv2rdf.xsl
 saxon -s:temp/code_cpv_suppl_2008.xml -xsl:cpv2rdf.xsl
 
-echo "\n%% Converting data to Turtle..."
+notify "Converting data to Turtle..."
 rapper -i rdfxml -o turtle temp/cpv-2008.xml > temp/cpv-2008.ttl
 rapper -i rdfxml -o turtle temp/cpv-2008-supplement.xml > temp/cpv-2008-supplement.ttl
 rapper -i rdfxml -o turtle temp/cpv-2008-metadata.xml > temp/cpv-2008-metadata.ttl
 
-echo "\n%% Generating additional data..."
+notify "Generating additional data..."
 arq --data temp/cpv-2008.ttl --query queries/add_skos_narrowerTransitive.rq > temp/skos_narrowerTransitive_links.ttl
 
-echo "\n%% Merging data..."
+notify "Merging data..."
 cat temp/cpv-2008.ttl temp/cpv-2008-supplement.ttl temp/skos_narrowerTransitive_links.ttl > temp/whole-cpv-2008.ttl
 
-echo "\n%% Calculating data statistics..."
+notify "Calculating data statistics..."
 arq --data temp/whole-cpv-2008.ttl --query queries/generateVoid.rq > temp/void.ttl
 
-echo "\n%% Merging metadata..."
+notify "Merging metadata..."
 cat temp/cpv-2008-metadata.ttl temp/void.ttl > temp/whole-cpv-2008-metadata.ttl
 
 test -d "output" || mkdir output
 
-echo "\n%% Reserializing..."
+notify "Reserializing..."
 rapper -i turtle -o turtle temp/whole-cpv-2008.ttl > output/cpv-2008.ttl
 rapper -i turtle -o turtle temp/whole-cpv-2008-metadata.ttl > output/cpv-2008-metadata.ttl
 
 # Cleanup
 rm -r temp
 
-echo "\n%% Converted CPV is in the 'output' directory"
+notify "Converted CPV is in the 'output' directory"
